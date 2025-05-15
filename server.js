223c72c40b40c48f90b917cc519c6fed570bfd9b
senneb31 <sennebrussel@gmail.com>
2025-05-15 23:22:40 +0200

    error code aan de server toegevoegd #11

--- server.js
+++ server.js
@@ -112,4 +112,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.status(404).send("Deze pagina bestaat niet!")
+//Deze code zorgt ervoor dat alle ongeldige URL leiden naar een error pagina
+app.use(function (request,response) {
+  response.status(404).render('error')
 })
